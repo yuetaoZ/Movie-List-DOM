bd77b7fdad409d5918fef6fbccef31f37d0d1883
YuetaoZ <60456187+yuetaoZ@users.noreply.github.com>
2021-08-09 00:29:14 -0500

    Updated liked list and like button

--- script.js
+++ script.js
@@ -66,8 +66,15 @@ const createMovieCard = (data) => {
   movieRatingArea.appendChild(starIcon);
   movieRatingArea.appendChild(movieRating);
   const movieHeartIcon = document.createElement("div");
-  movieHeartIcon.className = "ion-ios-heart-outline";
-  movieHeartIcon.id = "movie-heart-icon";
+  movieHeartIcon.classList.add("movie-heart-icon");
+  const same = model.likedMovieList.filter((movie) => {
+    return movie.id == data.id;
+  });
+  if (same.length !== 0) {
+    movieHeartIcon.classList.add("ion-ios-heart");
+  } else {
+    movieHeartIcon.classList.add("ion-ios-heart-outline");
+  }
   movieFooter.appendChild(movieRatingArea);
   movieFooter.appendChild(movieHeartIcon);
   movieCard.appendChild(movieImg);
@@ -76,20 +83,89 @@ const createMovieCard = (data) => {
   return movieCard;
 };
 
+const handleMovieTitleController = (e) => {
+  const movieDetailsContainer = document.querySelector(
+    ".movie-details-container"
+  );
+
+  movieDetailsContainer.style.display = "block";
+};
+
+const handleMovieHeartIconController = (e) => {
+  const heartIcon = e.target;
+  console.log(`heartIcon`, heartIcon);
+  heartIcon.classList.toggle("ion-ios-heart-outline");
+  heartIcon.classList.toggle("ion-ios-heart");
+  const currMovieCard = heartIcon.parentNode.parentNode;
+  const indexRecord = currMovieCard.indexRecord;
+  const movieData = model.movieList[indexRecord];
+
+  if (!heartIcon.classList.contains("ion-ios-heart")) {
+    model.likedMovieList = model.likedMovieList.filter((movie) => {
+      return movie.id != movieData.id;
+    });
+  } else {
+    model.likedMovieList.push(movieData);
+  }
+};
+
+const handleMovieCardAreaController = (e) => {
+  console.log(`e.target`, e.target);
+  if (e.target.classList.contains("movie-title")) {
+    handleMovieTitleController(e);
+  } else if (e.target.classList.contains("movie-heart-icon")) {
+    handleMovieHeartIconController(e);
+  }
+};
+
+const handleMovieDetailsCloseController = () => {
+  const movieDetailsContainer = document.querySelector(
+    ".movie-details-container"
+  );
+
+  movieDetailsContainer.style.display = "none";
+};
+
+const handleNavBarController = (e) => {
+  if (e.target.classList.contains("home-button")) {
+    loadDefaultData();
+  } else if (e.target.classList.contains("liked-list-button")) {
+    model.movieList = model.likedMovieList;
+    updateView();
+  }
+};
+
+const updateView = () => {
+  const movieCardContainer = getMovieCardArea();
+  movieCardContainer.innerHTML = "";
+  const movieList = model.movieList;
+  for (let i = 0, len = movieList.length; i < len; i++) {
+    const movieCard = createMovieCard(model.movieList[i]);
+    movieCard.indexRecord = i;
+    movieCardContainer.appendChild(movieCard);
+  }
+};
+
 const loadDefaultData = () => {
   getGereList().then(() => {
     getPopularMovieList(1).then(() => {
-      const movieCardContainer = getMovieCardArea();
-      const movieList = model.movieList;
-      for (let i = 0, len = movieList.length; i < len; i++) {
-        const movieCard = createMovieCard(model.movieList[i]);
-        movieCardContainer.appendChild(movieCard);
-      }
+      updateView();
     });
   });
 };
 
-const loadEvents = () => {};
+const loadEvents = () => {
+  const movieCard = document.querySelector(".movie-card-area");
+  const movieDetailsClose = document.querySelector(".movie-details-close-icon");
+  const navBar = document.querySelector(".nav-bar");
+
+  movieCard.addEventListener("click", handleMovieCardAreaController);
+  movieDetailsClose.addEventListener(
+    "click",
+    handleMovieDetailsCloseController
+  );
+  navBar.addEventListener("click", handleNavBarController);
+};
 
 loadDefaultData();
 loadEvents();
